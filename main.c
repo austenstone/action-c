a443d386bf307c0e5d9c6c260dd59bcf9e604a08
Austen Stone <austenstone@github.com>
2022-07-06 23:14:48 -0400

    test

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    int i, max = atoi(argv[1]);
-    if (argc < 1) return -1;
-    for (i = 0; i < max; i++) {
-        printf("%d", i);
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    if (num % 2 == 0 && num > 2) return 0;
+    for (int i = 3; i < num / 2; i+=2) {
+        if (num % i == 0) return 0;
     }
+    return 1;
+}
+
+int largest_prime_factor(int num) {
+    int largest = 0;
+    for (int i = 0; i < num; i++) {
+        if (is_prime(i)) largest = i;
+    }
+    return largest;
+}
+
+int main(int argc, char **argv) {
+    int number = 0;
+    if (argc < 1) {
+        printf("No arguments.");
+        return -1;
+    } else {
+        number = atoi(argv[1]);
+    }
+    printf("::set-output name=largest_prime_factor::%d\n", largest_prime_factor(number));
+    return 0;
 }
